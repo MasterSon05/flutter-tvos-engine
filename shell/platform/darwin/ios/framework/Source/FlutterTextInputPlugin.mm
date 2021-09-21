12c9dfa59e94132a6b1a8fed942a8f96d08534ff
Aleksandr_Denisov <aleksandr_denisov@epam.com>
2021-09-21 13:05:35 +0300

    view controller updates

--- shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
@@ -787,11 +787,13 @@ static BOOL IsSelectionRectCloserToPoint(CGPoint point,
     _smartDashesType = UITextSmartDashesTypeYes;
     _selectionRects = [[NSArray alloc] init];
 
+  #if !(defined(TARGET_OS_TV) && TARGET_OS_TV)
     if (@available(iOS 14.0, *)) {
       UIScribbleInteraction* interaction =
           [[[UIScribbleInteraction alloc] initWithDelegate:self] autorelease];
       [self addInteraction:interaction];
     }
+  #endif
   }
 
   return self;
